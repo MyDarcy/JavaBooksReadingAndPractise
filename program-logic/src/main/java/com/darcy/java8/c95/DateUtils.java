e88502d9f24e630503ec7602462ad27c0e5346aa
darcy.q.cs <darcy.q.cs@gmail.com>
2021-02-08 23:47:34 +0800

    comment

--- program-logic/src/main/java/com/darcy/java8/c95/DateUtils.java
+++ program-logic/src/main/java/com/darcy/java8/c95/DateUtils.java
@@ -17,9 +17,85 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
+/**
+ *  Instant：表示时刻，不直接对应年月日信息，需要通过时区转换；
+ *  LocalDateTime：表示与时区无关的日期和时间，不直接对应时刻，需要通过时区转换；
+ *  ZoneId/ZoneOffset：表示时区；
+ *  LocalDate：表示与时区无关的日期，与LocalDateTime相比，只有日期，没有时间信息；
+ *  LocalTime：表示与时区无关的时间，与LocalDateTime相比，只有时间，没有日期信息；
+ *  ZonedDateTime：表示特定时区的日期和时间。
+ *
+ *  1. Instant表示时刻
+ *  Instant.now()
+ *  Instant.ofEpochMilli(System.currentTimeMillis())
+ *  Date也表示时刻，Instant和Date可以通过纪元时相互转换
+ *
+ *  2. LocalDateTime表示与时区无关的日期和时间
+ *
+ *  3. ZoneId/ZoneOffset
+ *  LocalDateTime不能直接转为时刻Instant，转换需要一个参数ZoneOffset,ZoneOffset表示相对于格林尼治的时区差，北京是+08:00
+ *  给定一个时刻，使用不同时区解读，日历信息是不同的，Instant有方法根据时区返回一个ZonedDateTime
+ *  ZoneOffset是ZoneId的子类，可以根据时区差构造。
+ *
+ *  4. 可以认为LocalDateTime由两部分组成，一部分是日期LocalDate，另一部分是时间LocalTime
+ *
+ *  5. ZonedDateTime表示特定时区的日期和时间
+ *  Local-DateTime内部不会记录时区信息，只会单纯记录年月日时分秒等信息，而ZonedDateTime除了记录日历信息，还会记录时区，它的其他大部分构建方法都需要显式传递时区
+ *  ZonedDateTime可以直接转换为Instant
+ *
+ *  6. 修改时期和时间有两种方式，一种是直接设置绝对值，另一种是在现有值的基础上进行相对增减操作，
+ *  Java 8的大部分类都支持这两种方式。
+ *  另外，Java 8的大部分类都是不可变类，修改操作是通过创建并返回新对象来实现的，原对象本身不会变。
+ *
+ *
+ */
 public class DateUtils {
 
 	public static final DateTimeFormatter datetimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+	public static void createInstant() {
+		// instant创建方式
+		Instant instant1 = Instant.now();
+		Instant instant2 = Instant.ofEpochMilli(System.currentTimeMillis());
+		System.out.println(instant1);
+		System.out.println(instant2);
+		// instant & date 互转
+		Instant.ofEpochMilli(new Date().getTime());
+		new Date(instant1.toEpochMilli());
+	}
+
+	public static void createLocalDate() {
+		// LocalDate创建方式
+		LocalDate localDate1 = LocalDate.now();
+		LocalDate localDate2 = LocalDate.of(2021, 2, 7);
+		System.out.println(localDate1);
+		System.out.println(localDate2);
+		// get
+		System.out.println(localDate1.getYear());
+		System.out.println(localDate1.getMonthValue());
+		System.out.println(localDate1.getMonth()); // Month
+		System.out.println(localDate1.getDayOfMonth());
+		System.out.println(localDate1.getDayOfWeek()); // DayOfWeek
+		System.out.println(localDate1.getDayOfYear());
+	}
+
+	public static void createZone() {
+		// 转换一个LocalDateTime为北京的时刻
+		Instant instantFromBeijingNow = LocalDateTime.now().toInstant(ZoneOffset.of("+08:00"));
+		System.out.println(instantFromBeijingNow);
+		ZoneId beijingZoneId = ZoneId.of("GMT+08:00");
+		// 给定一个时刻，使用不同时区解读，日历信息是不同的
+		System.out.println(instantFromBeijingNow.atZone(ZoneId.of("GMT+00:00")));
+		System.out.println(instantFromBeijingNow.atZone(ZoneId.of("GMT+01:00")));
+	}
+
+	public static void crateZonedDateTime() {
+		ZonedDateTime zonedDateTime1 = ZonedDateTime.of(LocalDateTime.now(), ZoneId.systemDefault());
+		System.out.println(zonedDateTime1);
+
+		Instant zonedDateTime2Instant = ZonedDateTime.now().toInstant();
+		System.out.println(zonedDateTime2Instant);
+	}
 
 	public static void localDateTimeDemo(){
 		LocalDateTime ldt3 = LocalDateTime.of(2017, 7, 11, 20, 45, 5);
@@ -197,6 +273,24 @@ public class DateUtils {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		adjustDateTime6();
+		/**
+		 * 2021-02-07T05:30:12.875Z
+		 * 2021-02-07
+		 * 2021-02-07T13:30:12.876+08:00[Asia/Shanghai]
+		 */
+		System.out.println();
+		System.out.println(Instant.now());
+		System.out.println(LocalDate.now());
+		System.out.println(ZonedDateTime.now());
+
+		System.out.println();
+		createInstant();
+		System.out.println();
+		createLocalDate();
+		System.out.println();
+		createZone();
+		System.out.println();
+		crateZonedDateTime();
 	}
 
 }
